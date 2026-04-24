eecad90f49a5fb8e02962892fed5c2efce358382
Ryan Bradshaw <bradshr@bc.edu>
2026-04-24 12:40:26 -0400

    Initial Commit

---
+++ GuessThatTune/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GuessThatTune
+//
+//  Created by Ryan Bradshaw on 4/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuessThatTune/GuessThatTuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessThatTuneApp.swift
+//  GuessThatTune
+//
+//  Created by Ryan Bradshaw on 4/24/26.
+//
+
+import SwiftUI
+
+@main
+struct GuessThatTuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
